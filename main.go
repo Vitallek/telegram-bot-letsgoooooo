5e02d5ec1e7992391c186f73c13d95ff89b942d8
Vitallek <vitalikpozitiff@gmail.com>
2022-04-22 09:10:51 +0300

    weather

--- main.go
+++ main.go
@@ -9,15 +9,9 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 )
-
-type Location struct {
-	// Longitude as defined by sender
-	Longitude float64 `json:"longitude"`
-	// Latitude as defined by sender
-	Latitude float64 `json:"latitude"`
-}
 
 func main() {
 	pref := tele.Settings{
@@ -33,6 +27,60 @@ func main() {
 
 	b.Handle("/hello", func(ctx tele.Context) error {
 		return ctx.Send("Hello from fucking GO language, motherfucker")
+	})
+
+	b.Handle("/jw", func(ctx tele.Context) error {
+		msg := ctx.Message().Text
+		cityNameSplitArray := strings.Fields(msg)
+		cityName := cityNameSplitArray[1]
+		fmt.Print(cityName + "\n")
+		responseCities, err := http.Get(`https://api.geoapify.com/v1/geocode/autocomplete?text=` +
+			cityName +
+			`&type=city&limit=5&apiKey=` + os.Getenv("geoapifyToken"))
+
+		if err != nil {
+			fmt.Print(err.Error())
+			return ctx.Send("No cities found")
+		}
+
+		responseCitiesData, err := ioutil.ReadAll(responseCities.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		latTmplt, err := jsonparser.GetFloat(responseCitiesData, "features", "[0]", "properties", "lat")
+		lonTmplt, err := jsonparser.GetFloat(responseCitiesData, "features", "[0]", "properties", "lon")
+
+		responseWeather, err := http.Get(`https://api.openweathermap.org/data/2.5/weather?lat=` +
+			strconv.FormatFloat(float64(latTmplt), 'f', 10, 64) +
+			`&lon=` +
+			strconv.FormatFloat(float64(lonTmplt), 'f', 10, 64) +
+			`&limit=1&units=metric&lang=ru&appid=` + os.Getenv("openWeatherToken"))
+
+		if err != nil {
+			fmt.Print(err.Error())
+		}
+		responseWeatherData, err := ioutil.ReadAll(responseWeather.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		cityNameTmplt, err := jsonparser.GetString(responseWeatherData, "name")
+		weatherDescTmplt, err := jsonparser.GetString(responseWeatherData, "weather", "[0]", "description")
+		tempTmplt, err := jsonparser.GetFloat(responseWeatherData, "main", "temp")
+		feelsTmplt, err := jsonparser.GetFloat(responseWeatherData, "main", "feels_like")
+		humidityTmplt, err := jsonparser.GetFloat(responseWeatherData, "main", "humidity")
+		windTmplt, err := jsonparser.GetFloat(responseWeatherData, "wind", "speed")
+
+		sendTemplate := "\n\nПогода в городе " + cityNameTmplt + " сейчас:\n\n" +
+			"Облачность: " + weatherDescTmplt +
+			"\nТемпература :" + strconv.FormatFloat(tempTmplt, 'f', 0, 64) +
+			"°C \nощущается как " +
+			strconv.FormatFloat(feelsTmplt, 'f', 0, 64) + " °C\n" +
+			"\nВлажность: " + strconv.FormatFloat(humidityTmplt, 'f', 0, 64) + "%" +
+			"\nВетер:" + strconv.FormatFloat(windTmplt, 'f', 2, 64) + "м/c"
+
+		return ctx.Send(string(sendTemplate))
 	})
 
 	b.Handle(tele.OnLocation, func(ctx tele.Context) error {
@@ -86,7 +134,8 @@ func main() {
 			"\n\nБлижайший населённый пункт - " + string(cityNameTmplt) +
 			"\n\nПогода по координатам сейчас:\n\n" +
 			"Облачность: " + weatherDescTmplt +
-			"\nТемпература:" + strconv.FormatFloat(tempTmplt, 'f', 2, 64) + "°C, ощущается как " +
+			"\nТемпература :" + strconv.FormatFloat(tempTmplt, 'f', 2, 64) +
+			"°C \nощущается как " +
 			strconv.FormatFloat(feelsTmplt, 'f', 2, 64) + " °C\n" +
 			"\nВлажность: " + strconv.FormatFloat(humidityTmplt, 'f', 0, 64) + "%" +
 			"\nВетер:" + strconv.FormatFloat(windTmplt, 'f', 2, 64) + "м/c"
